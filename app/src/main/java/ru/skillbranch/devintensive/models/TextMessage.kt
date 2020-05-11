11440d16830c4b97ad9b61537d878788636b4e13
Stas188 <wowfaust@gmail.com>
2020-05-12 00:23:03 +0300

    errr try2

--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -4,16 +4,15 @@ package ru.skillbranch.devintensive.models
 import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
-class TextMessage (
+class TextMessage(
     id: String,
     from: User?,
     chat: Chat,
     isIncoming: Boolean = false,
-    date: Date,
-    var text:String?):
-    BaseMessage(id, from, chat, isIncoming, date) {
+    date:Date = Date(),
+    var text:String
+) : BaseMessage(id, from, chat, isIncoming, date) {
 
-    override fun formatMessage(): String = "id$id ${from?.firstName} ${if(isIncoming)"получил" else
-        "отправил"} сообщеник \"$text\" ${date.humanizeDiff()}"
-
+    override fun formatMessage(): String = "$id ${from?.firstName} " +
+            "${if (isIncoming) "получил" else "отправил"} сообщение \"$text\" ${date.humanizeDiff()}"
 }
--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -4,59 +4,56 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import ru.skillbranch.devintensive.extensions.*
-import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.*
 import ru.skillbranch.devintensive.utils.Utils
 import java.text.SimpleDateFormat
 import java.util.*
 
 class ExampleUnitTest {
     @Test
-    fun stripHtmlTest() {
-        /* skillBranch tests */
-        assertEquals("Образовательное IT-сообщество Skill Branch",
-            "<p class=\"title\">Образовательное IT-сообщество Skill Branch</p>".stripHtml())
-        assertEquals("Образовательное IT-сообщество Skill Branch",
-            "<p>Образовательное       IT-сообщество Skill Branch</p>".stripHtml())
-//
-//        /* additional tests */
-        assertEquals("single", "&amp;&lt;&gt;single&#39;&quot;".stripHtml())
-        assertEquals("", "&amp;&lt;&gt;&#39;&quot;".stripHtml())
-        assertEquals(" ", "&amp;&lt;&gt;    &#39;&quot;".stripHtml())
-        assertEquals("1978", "<path fill=\"Color\" d=\"M11.63 10z\"></svg><span>1978</span>".stripHtml())
-        assertEquals("", "&gt;<head>&#39;&quot;</head>".stripHtml())
-        assertEquals(" ", "&gt;<head> &quot; </head>".stripHtml())
-        assertEquals("&игра; amp lt &gt 39; meters ()quot;", "&игра; amp lt &gt 39; meters ()quot;".stripHtml())
-        assertEquals(" one two ", "  one   two ".stripHtml())
-        assertEquals("null", "null".stripHtml())
-        val longHtml = """
-            <TD valign="top" style="padding-bottom:15px;"> <b>line1<b> </TD>
-            <TD valign="top"> <span class="HeadTitleNews"> line2</span>
-            <img src='http://2011WaterpoloF.jpg' >
-            <div style="margin: 0in 0in 0pt">line3</div>
-       """.trimIndent()
-        assertEquals(" line1 \n line2\n\nline3", longHtml.stripHtml())
-    }
+    fun makeMessageTest(){
+        @Test
+        fun makeUserTest(){
+            assertEquals(User("0", "Иван", "Кузьмин").firstName, User.makeUser("Иван Кузьмин").firstName)
+            assertEquals(User("0", "Иван", "Кузьмин").lastName, User.makeUser("Иван Кузьмин").lastName)
+        }
 
-    @Test
-    fun truncateTest() {
-        /* skillBranch tests */
-        assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
-        assertEquals("Bender Bending...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
-        assertEquals("A", "A     ".truncate(3))
+        @Test
+        fun builderTest(){
+            val date = Date()
 
-//        /* additional tests */
-        assertEquals("too lo...", "   too long line with lots of spaces before".truncate(6))
-        assertEquals("too short", "too short".truncate(20))
-        assertEquals("12345", "12345".truncate(5))
-        assertEquals("1234...", "12345".truncate(4))
-        assertEquals("12345", "12345  ".truncate(5))
-        assertEquals("tab", "tab    ".truncate(5))
-        assertEquals("dots......", "dots... a lot".truncate(7))
-        assertEquals("abc", "abc    ".truncate(15))
-        assertEquals("123456...", "123456789".truncate(6))
-        assertEquals("123456789", "123456789".truncate(9))
-        assertEquals("Bender Bending R...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate())
-        assertEquals("Bender Bending...", "Bender Bending Rodriguez — дословно «Сгибальщик Сгибающий Родригес»".truncate(15))
-        assertEquals("1", "1     ".truncate(3))
+            val user1 = User(
+                "5",
+                "Никола",
+                "Тесла",
+                null,
+                0,
+                1000,
+                date.add(-2, TimeUnits.DAY),
+                false)
+
+            val user2 = User.Builder().id("5")
+                .firstName("Никола")
+                .lastName("Тесла")
+                .avatar(null)
+                .rating(0)
+                .respect(1000)
+                .lastVisit(date.add(-2, TimeUnits.DAY))
+                .isOnline(false)
+                .build()
+
+            val user3 = User.Builder().build()
+
+            assertEquals(user1, user2)
+            assertTrue(user3 is User)
+            assertNotEquals(null, user3.id)
+            assertEquals(null, user3.firstName)
+            assertEquals(null, user3.lastName)
+            assertEquals(null, user3.avatar)
+            assertEquals(0, user3.rating)
+            assertEquals(0, user3.respect)
+            assertNotEquals(null, user3.lastVisit)
+            assertFalse(user3.isOnline)
+        }
     }
 }
